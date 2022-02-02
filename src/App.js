243316d3208810f3eb4d5c219eca1da55c602f1f
Vinicius Matos de Mendonça <concursandopro1@gmail.com>
2022-02-02 11:44:22 -0300

    correção no nome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import { useState } from "react";
-import "./app.css";
+import "./App.css";
 
 export default function App() {
-  const [peso, setPeso] = useState('');
-  const [altura, setAltura] = useState('');
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
 
-  const [mensagem, setMensagem] = useState('');
+  const [mensagem, setMensagem] = useState("");
 
-  function calcularIMC () {
+  function calcularIMC() {
     const alt = altura / 100;
     const imc = peso / (alt * alt);
 
-    if(imc < 18.6) {
-      setMensagem("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
-    } else if(imc >= 18.6 && imc < 24.9) {
-      setMensagem("Peso ideal! Seu IMC: " + imc.toFixed(2))
-    } else if(imc >=24.9 && imc < 34.9) {
-      setMensagem("Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2))
-    } else if(imc > 34.9) {
-      setMensagem("Cuidado, Obesidade! Seu IMC: " + imc.toFixed(2))
+    if (imc < 18.6) {
+      setMensagem("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2));
+    } else if (imc >= 18.6 && imc < 24.9) {
+      setMensagem("Peso ideal! Seu IMC: " + imc.toFixed(2));
+    } else if (imc >= 24.9 && imc < 34.9) {
+      setMensagem(
+        "Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2)
+      );
+    } else if (imc > 34.9) {
+      setMensagem("Cuidado, Obesidade! Seu IMC: " + imc.toFixed(2));
     }
   }
 
@@ -27,8 +29,18 @@ export default function App() {
       <h1>Calculadora IMC</h1>
       <span>Vamos calcular o seu imc</span>
       <div className="area-input">
-        <input type="number" placeholder="Peso em (kg) Ex: 90" value={peso} onChange={(e) => setPeso(e.target.value)} />
-        <input type="number" placeholder="Altura em (cm) Ex: 180" value={altura} onChange={(e) => setAltura(e.target.value)} />
+        <input
+          type="number"
+          placeholder="Peso em (kg) Ex: 90"
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Altura em (cm) Ex: 180"
+          value={altura}
+          onChange={(e) => setAltura(e.target.value)}
+        />
 
         <button onClick={calcularIMC}>Calcular</button>
       </div>
